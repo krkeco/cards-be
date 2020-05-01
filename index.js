8652a43293d302570184f15157ce6eec892c84fa
KC <krkeco@gmail.com>
2020-05-01 11:34:20 -0700

    remove old api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var graphqlApi = require('./graphql.js')
 
-var gameXpressApi = require('./XPApi.js')
+// var gameXpressApi = require('./XPApi.js')
 
-const sim = require('./objects/Simulation.js');
+// const sim = require('./objects/Simulation.js');
 // let simulation = new sim.Series(10);
